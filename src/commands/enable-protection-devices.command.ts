4f5583ed646a64cf18cdb1dc2d382122e56a6625
Shakhzod Khodjaev <sop.ugc2@gmail.com>
2024-02-29 20:27:45 +0500

    add abortsignal, and catch error from fetch

--- src/commands/enable-protection-devices.command.ts
+++ src/commands/enable-protection-devices.command.ts
@@ -5,13 +5,20 @@ export class EnableProtectionDevicesCommand {
     devices: Device[]
   ): Promise<PromiseSettledResult<DeviceCommandResult>[]> {
     const requests = devices.map(async (device) => {
-      const response = await fetch(
-        `${device.ip}:${device.port}/protection/enable`
-      );
-      if (!response.ok) {
-        return { ...device, success: false, error: await response.text() };
-      } else {
-        return { ...device, success: true, data: await response.json() };
+      try {
+        const response = await fetch(
+          `${device.ip}:${device.port}/protection/enable`,
+          { signal: AbortSignal.timeout(10000) }
+        );
+
+        if (!response.ok) {
+          return { ...device, success: false, error: await response.text() };
+        } else {
+          return { ...device, success: true, data: await response.json() };
+        }
+      } catch (error) {
+        const err = error as Error;
+        return { ...device, success: false, error: err.message };
       }
     });
 
